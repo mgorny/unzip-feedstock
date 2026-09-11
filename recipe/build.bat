9d88edee7a07a381ddb477ce3e7c3c740d63ce05
Michał Górny <mgorny@gentoo.org>
2026-09-11 18:15:10 +0200

    Enable building on Windows
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

---
+++ recipe/build.bat
@@ -0,0 +1,9 @@
+FOR /F %%P IN (patches\series) DO (
+    patch -p1 -i %%P
+    IF !errorlevel! neq 0 EXIT /b !errorlevel!
+)
+
+CD win32
+IF !errorlevel! neq 0 EXIT /b !errorlevel!
+nmake
+IF !errorlevel! neq 0 EXIT /b !errorlevel!
